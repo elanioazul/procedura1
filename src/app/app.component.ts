dc629e097e01b911ee820abeabadc7af0f8877f8
hugo <hugo.herrador@gmail.com>
2021-09-11 19:43:24 +0200

    Basic css framing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'procedura1';
+  innerWidth: any;
+  isShown: boolean;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    this.isShown = true;
+    console.log(`la anchura es: ${this.innerWidth}px`);
+    if (this.innerWidth < 768) {
+      this.isShown = false;
+    }
+  }
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public varYear;
+
   constructor() { }
 
   ngOnInit(): void {
+    let date = new Date();
+    let year = date.getFullYear();
+    this.varYear = year;
   }
 
 }
